5f44ea80b844c33b5a9be472ac7fffd268e9500b
Chris Usher <usher@cfht.hawaii.edu>
2019-07-15 17:14:52 -1000

    Updated to verison 019.

--- cfht/cfhttrigger.py
+++ cfht/cfhttrigger.py
@@ -7,7 +7,7 @@ from .distribution import ProductDistributorFactory, distribute_raw_file
 from .fileselector import CfhtFileSelector
 from .steps import CfhtDrsSteps
 
-TRIGGER_VERSION = '018'
+TRIGGER_VERSION = '019'
 
 
 class CfhtHandler(AbstractCustomHandler):
